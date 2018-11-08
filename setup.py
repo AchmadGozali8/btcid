8ec630e7ce001e6519b1325ec579e706c747d4b6
AcmadGozali8 <achmadg6@gmail.com>
2018-11-08 18:42:53 +0700

    new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name="btcid",
+	version="1.0",
+	author_email="achmadg6@gmail.com",
+	description="Indodax API",
+	install_requires = [
+		"pandas==0.23.4",
+		"requests==2.20.0"
+	],	
+)
